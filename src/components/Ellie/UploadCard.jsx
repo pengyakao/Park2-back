369dc11414d2dd32ed31b139770ce34b7608e3ac
Ellie <apple1994513@gmail.com>
2022-07-01 14:37:43 +0800

    0701_14:37_Ellie

--- src/components/Ellie/UploadCard.jsx
+++ src/components/Ellie/UploadCard.jsx
@@ -15,29 +15,9 @@ import { AccessAlarm, NoEncryption, ThreeDRotation } from '@mui/icons-material'
 export default function BasicSwitches() {
     return (
         <Card sx={{ maxWidth: 252, minHeight: 223, border: '5px solid #F4F4F4' }}>
-            {/* <CardMedia
-                component="img"
-                alt="store-img"
-                height="140"
-                image="/img/store/MOLINO.jpeg"
-            /> */}
-            <button id="uploadIcon">
+            <Button id="uploadIcon" sx={{ minWidth: 252, minHeight: 223 }}>
                 <AddCircleOutlineIcon />
-            </button>
-            {/* <Typography gutterBottom fontSize="14" margin="5px" component="div">
-                活動名稱
-            </Typography> */}
-            {/* <div>
-                <CardActions margin="-5px">
-                    <Button size="small">編輯</Button>
-                    <Button size="small">刪除</Button>
-                    <Stack direction="row" spacing={1} alignItems="center">
-                        <Typography>Off</Typography>
-                        <AntSwitch defaultChecked inputProps={{ 'aria-label': 'ant design' }} />
-                        <Typography>On</Typography>
-                    </Stack>
-                </CardActions>
-            </div> */}
+            </Button>
         </Card>
     )
 }
