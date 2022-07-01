1331de02f66951ba4f0a1ff8cd721cbada2e275c
Ellie <apple1994513@gmail.com>
2022-07-01 16:18:26 +0800

    0701_Ellie

--- src/page/store/store_each.jsx
+++ src/page/store/store_each.jsx
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
+import './store_each.css'
+import Aside from '../../components/Crystal/Aside'
+import Header from '../../components/Crystal/Header'
 
 class Store_each extends Component {
-    state = {  } 
-    render() { 
+    state = {}
+    render() {
         return (
-            <h3>店家管理-單一個店家編輯頁面</h3>
-        );
+            <div className="all-cantainer">
+                <Header></Header>
+                <div className="header-hight"></div>
+                <Aside></Aside>
+                <div className="SC-container">
+                    <h1>1店家列表</h1>
+                    <h1>2店家列表</h1>
+                    <h1>3店家列表</h1>
+                    <h1>4店家列表</h1>
+                    <h1>5店家列表</h1>
+                </div>
+            </div>
+        )
     }
 }
- 
-export default Store_each;
+
+export default Store_each
