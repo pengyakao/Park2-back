bd89e041d89891cd228f30b22408bdfc7113caf3
Ellie <apple1994513@gmail.com>
2022-07-06 13:58:27 +0800

    0705_Ellie

--- src/page/store/store_each.jsx
+++ src/page/store/store_each.jsx
@@ -6,16 +6,16 @@ import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
-import TextareaAutosize from '@mui/material/TextareaAutosize'
-import FormGroup from '@mui/material/FormGroup'
-import FormControlLabel from '@mui/material/FormControlLabel'
+import TextareaAutosize from './TextareaAutosize'
 import Checkbox from '@mui/material/Checkbox'
-import UploadButtons from '../../components/Ally/UploadButtons'
-import ComboBox from '../../components/Ellie/ComboBox'
-import Autocomplete from '@mui/material/Autocomplete'
-import UploadMore from '../../components/Ally/UploadMore'
-import TimeOption from '../../components/Ellie/TimeOption'
-import CreditCardIcon from '@mui/icons-material/CreditCard'
+import UploadButtons from './UploadButtons'
+import ComboBox from './ComboBox'
+import UploadMore from './UploadMore'
+import TimeOption from './TimeOption'
+import Input_fb from './Input_fb'
+import Input_ig from './Input_ig'
+import Input_line from './Input_line'
+import './store_each.css'
 
 const theme = createTheme({
     palette: {
@@ -26,94 +26,65 @@ const theme = createTheme({
     },
 })
 
-const style1 = {
-    color: '#000',
-    'font-family': 'Noto Sans TC',
-    'font-weight': 500,
-    'letter-spacing': '1px',
-    'font-size': '20px',
-}
-
-const store_new = ({ add }) => {
-    const home_act_data = [
+const store_each = ({ add }) => {
+    const store_each_data = [
         {
-            home_act_id: 1,
-            act_id: 1,
-            home_act_sta: 1,
-            home_act_title: '活動名稱A',
-        },
-        {
-            home_act_id: 2,
-            act_id: 2,
-            home_act_sta: 0,
-            home_act_title: '活動名稱B',
-        },
-        {
-            home_act_id: 3,
-            act_id: 3,
-            home_act_sta: 1,
-            home_act_title: '活動名稱C',
-        },
-        {
-            home_act_id: 4,
-            act_id: 4,
-            home_act_sta: 0,
-            home_act_title: '活動名稱D',
+            sto_id: 1,
+            sto_name: 12333,
+            sto_img: 'https://placem.at/people',
         },
     ]
 
-    const act_data = [
-        {
-            act_id: '活動名稱A',
-            act_Sdate: '05/06',
-            act_Edate: '05/21',
-            act_Stime: '21:00',
-            act_Etime: '21:30',
-        },
-        {
-            act_id: '活動名稱B',
-            act_Sdate: '05/01',
-            act_Edate: '05/02',
-            act_Stime: '20:00',
-            act_Etime: '20:30',
-        },
-        {
-            act_id: '活動名稱C',
-            act_Sdate: '04/28',
-            act_Edate: '04/29',
-            act_Stime: '09:00',
-            act_Etime: '09:30',
-        },
-        {
-            act_id: '活動名稱D',
-            act_Sdate: '05/16',
-            act_Edate: '05/17',
-            act_Stime: '21:00',
-            act_Etime: '21:30',
-        },
-    ]
-    const time1 = {
-        border: '2px #F4F4F4 solid',
-        // display: 'flex',
-        justifyContent: 'center',
-        alignItem: 'center',
-    }
-    const time2 = {
-        display: 'flex',
-        // justifyContent: 'flex-start',
-        margin: '10px',
-    }
-    const time3 = {
-        margin: '20px',
-    }
+    // const new_data = [
+    //     {
+    //         act_id: '活動名稱A',
+    //         act_Sdate: '05/06',
+    //         act_Edate: '05/21',
+    //         act_Stime: '21:00',
+    //         act_Etime: '21:30',
+    //     },
+    //     {
+    //         act_id: '活動名稱B',
+    //         act_Sdate: '05/01',
+    //         act_Edate: '05/02',
+    //         act_Stime: '20:00',
+    //         act_Etime: '20:30',
+    //     },
+    //     {
+    //         act_id: '活動名稱C',
+    //         act_Sdate: '04/28',
+    //         act_Edate: '04/29',
+    //         act_Stime: '09:00',
+    //         act_Etime: '09:30',
+    //     },
+    //     {
+    //         act_id: '活動名稱D',
+    //         act_Sdate: '05/16',
+    //         act_Edate: '05/17',
+    //         act_Stime: '21:00',
+    //         act_Etime: '21:30',
+    //     },
+    // ]
+    // const store_each = [
+    //     {
+    //         sto_pay1: '1',
+    //         sto_pay2: '0',
+    //         sto_pay3: '1',
+    //         sto_pay4: '0',
+    //         sto_pay5: '0',
+    //         sto_pay6: '1',
+    //         sto_pay7: '1',
+    //     },
+    // ]
     const label = { inputProps: { 'aria-label': 'Checkbox demo' } }
+
     return (
         <div>
             <div>
                 <LayOut />
                 <div className="bs_article">
                     <div style={{ width: '80%' }}>
-                        <h1>編輯店家資訊</h1>
+                        <h1>新增店家資訊</h1>
                         <Box
                             component="form"
                             sx={{
@@ -123,83 +94,132 @@ const store_new = ({ add }) => {
                             autoComplete="off"
                             direction="row"
                         >
-                            <div>
+                            <div className="storeMainImg">
                                 <h3>封面圖片</h3>
                                 <UploadButtons width={300}></UploadButtons>
                             </div>
-                            <h3>店家名稱</h3>
-                            <TextField id="outlined-basic" label="店家名稱" variant="outlined" required="true" />
-                            <h3>店家位置</h3>
-                            <ComboBox></ComboBox>
-                            <h3>聯絡電話</h3>
-                            <TextField
-                                id="outlined-basic"
-                                label="聯絡電話"
-                                placeholder="0912-345-678"
-                                variant="outlined"
-                                required="true"
-                            />
-                            <h3>
-                                店家圖片
-                                <span style={{ fontSize: '12px', fontWeight: '400' }}>（至少五張，最多八張）</span>
-                            </h3>
-                            <UploadMore></UploadMore>
-                            <h3>營業時間</h3>
-                            <div style={time1}>
-                                <div direction="row" style={time2}>
-                                    <div style={time3}>週一</div>
-                                    <TimeOption></TimeOption>
-                                    <div style={time3}>～</div>
-                                    <TimeOption></TimeOption>
-                                </div>
-                                <div direction="row" style={time2}>
-                                    <div style={time3}>週二</div>
-                                    <TimeOption></TimeOption>
-                                    <div style={time3}>～</div>
-                                    <TimeOption></TimeOption>
-                                </div>
-                                <div direction="row" style={time2}>
-                                    <div style={time3}>週三</div>
-                                    <TimeOption></TimeOption>
-                                    <div style={time3}>～</div>
-                                    <TimeOption></TimeOption>
-                                </div>
-                                <div direction="row" style={time2}>
-                                    <div style={time3}>週四</div>
-                                    <TimeOption></TimeOption>
-                                    <div style={time3}>～</div>
-                                    <TimeOption></TimeOption>
-                                </div>
-                                <div direction="row" style={time2}>
-                                    <div style={time3}>週五</div>
-                                    <TimeOption></TimeOption>
-                                    <div style={time3}>～</div>
-                                    <TimeOption></TimeOption>
-                                </div>
-                                <div direction="row" style={time2}>
-                                    <div style={time3}>週六</div>
-                                    <TimeOption></TimeOption>
-                                    <div style={time3}>～</div>
-                                    <TimeOption></TimeOption>
-                                </div>
-                                <div direction="row" style={time2}>
-                                    <div style={time3}>週日</div>
-                                    <TimeOption></TimeOption>
-                                    <div style={time3}>～</div>
-                                    <TimeOption></TimeOption>
-                                </div>
+                            <div className="storeName">
+                                <h3>店家名稱</h3>
+                                <TextField
+                                    id={store_each.sto_name}
+                                    label="店家名稱"
+                                    variant="outlined"
+                                    required="true"
+                                />
+                            </div>
+                            <div className="storeSet">
+                                <h3>店家位置</h3>
+                                <ComboBox></ComboBox>
+                            </div>
+                            <div className="storePhone">
+                                <h3>聯絡電話</h3>
+                                <TextField
+                                    id="outlined-basic"
+                                    label="聯絡電話"
+                                    placeholder="0912-345-678"
+                                    variant="outlined"
+                                    required="true"
+                                />
                             </div>
                             <div>
-                                <h3>支付方式</h3>
-                                <div direction="row" style={time1}>
-                                    <Checkbox {...label} defaultChecked id="pay" color="secondary" />
-                                    <label htmlFor="pay">
-                                        <CreditCardIcon></CreditCardIcon>
-                                    </label>
+                                <div className="storeMoreImg">
+                                    <h3>
+                                        店家圖片
+                                        <span style={{ fontSize: '12px', fontWeight: '400' }}>
+                                            （至少五張，最多八張）
+                                        </span>
+                                    </h3>
+                                    <UploadMore></UploadMore>
                                 </div>
                             </div>
-                            <h3>店家介紹</h3>
-                            <TextareaAutosize aria-label="empty textarea" style={{ height: 300 }} showCount />
+                            <div className="storetime">
+                                <h3>營業時間</h3>
+                                <div className="storetime_container">
+                                    <div>
+                                        <div>平日：</div>
+                                        <TimeOption></TimeOption>
+                                        <div>～</div>
+                                        <TimeOption></TimeOption>
+                                    </div>
+                                    <div>
+                                        <div>假日：</div>
+                                        <TimeOption></TimeOption>
+                                        <div>～</div>
+                                        <TimeOption></TimeOption>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="storepay">
+                                <h3>支付方式</h3>
+                                <div className="storepay_container">
+                                    <div>
+                                        <Checkbox {...label} checked={store_each.sto_pay1} id="sto_pay1" />
+                                        <label htmlFor="sto_pay1">
+                                            <img src="/img/icon/cash.svg" alt="現金支付" />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <Checkbox {...label} checked={store_each.sto_pay2} id="sto_pay2" />
+                                        <label htmlFor="sto_pay2">
+                                            <img src="/img/icon/card.svg" alt="信用卡" />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <Checkbox {...label} checked={store_each.sto_pay3} id="sto_pay3" />
+                                        <label htmlFor="sto_pay3">
+                                            <img src="/img/icon/linepay.png" alt="信用卡" />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <Checkbox {...label} checked={store_each.sto_pay4} id="sto_pay4" />
+                                        <label htmlFor="sto_pay4">
+                                            <img src="/img/icon/street.png" alt="街口" />
+                                        </label>
+                                    </div>
+
+                                    <div>
+                                        <Checkbox {...label} checked={store_each.sto_pay5} id="sto_pay5" />
+                                        <label htmlFor="sto_pay5">
+                                            <img src="/img/icon/icash.png" alt="悠遊卡" />
+                                        </label>
+                                    </div>
+
+                                    <div>
+                                        <Checkbox {...label} checked={store_each.sto_pay6} id="sto_pay6" />
+                                        <label htmlFor="sto_pay6">
+                                            <img src="/img/icon/Apple_Pay_logo.svg" alt="ApplePay" />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <Checkbox {...label} checked={store_each.sto_pay7} id="sto_pay7" />
+                                        <label htmlFor="sto_pay7">
+                                            <img src="/img/icon/taobao.png" alt="支付寶" />
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="fanspage">
+                                <h3>粉絲專頁</h3>
+                                <div className="fanspage-container">
+                                    <div>
+                                        <Input_fb />
+                                    </div>
+                                    <div>
+                                        <Input_ig />
+                                    </div>
+                                    <div>
+                                        <Input_line />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="storeintro">
+                                <h3>店家介紹</h3>
+                                <TextareaAutosize
+                                    aria-label="empty textarea"
+                                    style={{ height: 300, border: '2px solid #f4f4f4' }}
+                                    showCount
+                                />
+                            </div>
                         </Box>
                         <ThemeProvider theme={theme}>
                             <Stack
@@ -207,13 +227,13 @@ const store_new = ({ add }) => {
                                 direction="row"
                                 style={{ display: 'flex', 'justify-content': 'flex-end' }}
                             >
-                                <Button color="neutral" variant="outlined" href="/home/act">
+                                <Button color="neutral" variant="outlined" href="/store">
                                     取消
                                 </Button>
                                 <Button
                                     color="neutral"
                                     variant="contained"
-                                    href="/home/faq"
+                                    href="/store"
                                     onClick={() => {
                                         alert('送出')
                                     }}
@@ -227,21 +247,6 @@ const store_new = ({ add }) => {
             </div>
         </div>
     )
-
-    // const options = [
-    //     { label: 'The Godfather', id: 1 },
-    //     { label: 'Pulp Fiction', id: 2 },
-    // ]
-
-    const top100Filmsss = [
-        { label: 'The Shawshank Redemption', year: 1994 },
-        { label: 'The Godfather', year: 1972 },
-        { label: 'The Godfather: Part II', year: 1974 },
-        { label: 'The Dark Knight', year: 2008 },
-        { label: '12 Angry Men', year: 1957 },
-        { label: "Schindler's List", year: 1993 },
-        { label: 'Pulp Fiction', year: 1994 },
-    ]
 }
 
-export default store_new
+export default store_each
