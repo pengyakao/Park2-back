1a6c12d937cbbc7c778addb0ab6e8238972f1960
高鵬雅 <pengyagao@gmail.com>
2022-07-09 23:47:23 +0800

    0709_ally_活動card

--- src/page/ally_compontents.js
+++ src/page/ally_compontents.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import Button from '@mui/material/Button';
@@ -46,44 +45,5 @@ class Ally extends Component {
   getCarousel().then((result) => {
     console.log(result)
 });
-  }
-
-=======
-import * as React from 'react'
-import ReactDOM from 'react-dom/client'
-import Button from '@mui/material/Button'
-import { useState } from 'react'
-import UploadButtons from '../components/Ally/UploadButtons'
-import FAQlist from '../components/Ally/FAQlist'
-import UploadMore from '../components/Ally/UploadMore'
-import UploadFile from '../components/Ally/UploadFile'
-import BasicModal from '../components/Ellie/BasicModal'
-import { Component } from 'react'
-import ComboBox from '../components/Ellie/ComboBox'
-
-class Ally extends Component {
-    state = {}
-    render() {
-        return (
-            <React.Fragment>
-                <h3>FAQlist</h3>
-                <FAQlist />
-                <hr />
-                <h3>圖片上傳</h3>
-                <UploadButtons />
-                <hr />
-                <h3>多張圖片上傳</h3>
-                <UploadMore />
-                <hr />
-                <h3>合約文件上傳(限.pdf檔)</h3>
-                <UploadFile />
-
-                <h3>彈跳窗</h3>
-                <BasicModal />
-                <ComboBox />
-            </React.Fragment>
-        )
-    }
->>>>>>> 96eda72e829804eb251717bd447569d1d3bb941b
-}
+  }}
 export default Ally
