b21f0f362b03c4fd897827be225aa2cc4e9f6b79
“高鵬雅“ <pengyagao@gmail.com>
2022-07-18 21:26:36 +0800

    0718_ally_storeIsMain

--- src/api/url.js
+++ src/api/url.js
@@ -1,4 +1,5 @@
 // export const url = "http://192.168.0.161:3001"
 //教室
 // windows
-export var url = "http://10.0.101.137:3001"
+// export var url = "http://10.0.101.137:3001"
+export var url = "http://192.168.0.101:3001"
