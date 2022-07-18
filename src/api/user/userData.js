08f55db7e1c2fb1373dbfbffd478b34f4974c0b3
TyyyHo <ty.ho1126@gmail.com>
2022-07-18 16:46:29 +0800

    0718_userPermission

--- src/api/user/userData.js
+++ src/api/user/userData.js
@@ -4,9 +4,7 @@ import { url } from "../url.js";
 // 設定 baseUrl
 const reqObj = axios.create({
   // baseURL 設定的是自己測試的 IP位址:server埠號 (ex. http://192.168.0.112:3000)
-  // baseURL: 'http://10.0.101.137:3001',
   baseURL: url,
-
   header: {
     "Content-Type": "application/json",
   },
--- src/page/user/user.js
+++ src/page/user/user.js
@@ -4,19 +4,14 @@ import LayOutUser from '../../components/layout/LayOut_user';
 import Userlist from './user_list';
 import { useEffect } from 'react';
 
-
-
-
 // api
-import {token} from '../../api/user/token'
-import {data} from '../../api/user/data'
 import { getUser } from "../../api/user/userData";
 import { checkLogin } from '../../api/login/isLogin'
 
 export default function User() {
     const [data, setData] = React.useState([]);
 
-    var myToken = token
+
     var listData = data;
 
     useEffect(()=>{
@@ -31,20 +26,15 @@ export default function User() {
     },[])
 
 
-    // 判斷權限
-    if (myToken.user_id == 1) {
-        return (
-            <div>
-                <LayOutUser />
-                <div className='bs_article'>
-                    <h1>帳號管理</h1>
-                    <Userlist listData={listData} />
-                </div>
+    return (
+        <div>
+            <LayOutUser />
+            <div className='bs_article'>
+                <h1>帳號管理</h1>
+                <Userlist listData={listData} />
             </div>
-        )
-    } else {
-        return <Redirect to={`/user/${myToken.user_id}`} />;
-    }
+        </div>
+    )
 
    
 }
