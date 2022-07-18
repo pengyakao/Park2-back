f87b2c898a02313a9829efba6375136d284bdd7a
Eddie Lin <j951j951@gmail.com>
2022-07-18 19:20:50 +0800

    0718_eddie

--- src/api/url.js
+++ src/api/url.js
@@ -1,9 +1,4 @@
 // export const url = "http://192.168.0.161:3001"
 //教室
-<<<<<<< HEAD
-export var url = "http://192.168.0.161:3001"
-=======
-// export var url = "http://10.0.102.245:3001"
 // windows
 export var url = "http://10.0.101.137:3001"
->>>>>>> fe038563fb269c7f2aa0776efc64fb083f2c1644
